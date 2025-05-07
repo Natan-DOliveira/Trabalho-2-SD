12510d3a13025d599981f7f0903aea9fce21b8b1
Natan Duarte de Oliveira <natanduarteoliveira@gmail.com>
2025-05-07 09:04:29 -0300

    Update bullcow_game_top.sv

--- rtl/bullcow_game_top.sv
+++ rtl/bullcow_game_top.sv
@@ -2,8 +2,11 @@ module Bullcow_Game_Top (
     input logic clock,
     input logic reset,
     input logic enter,
-    input logic segment[7:0],
-    input logic display[7:0],
     input logic SW[15:0],
-    input logic LED[15:0]
+    output logic LED[15:0],
+    output logic segment[7:0],
+    output logic display[7:0]
 );
+
+
+
